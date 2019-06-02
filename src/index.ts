52070076ce3cb123c14d124d8fde599e9abc57ed
Yih Yang <yyngo788n88@hotmail.com>
2019-06-02 13:35:49 +0800

    fixed issue with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-const express = require( "express" );
+import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+  res.send( "Hello world!" );
 } );
 
 // start the Express server
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+  // tslint:disable-next-line:no-console
+  console.log( `server started at http://localhost:${ port }` );
 } );
